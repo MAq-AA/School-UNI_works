5b92abb9ccc2ad2620514e36de9e2ed7128cf47e
MAq-AA <artem.maksimov.017@mail.ru>
2025-01-27 18:28:45 +0300

    a

---
+++ !!math_dot_in_circle/Console_application/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class PointInCircle
+{
+    static void Main()
+    {
+        double distance, pointX, pointY, centerX, centerY, radius;
+        while (true)
+        {
+            try
+            {
+                Console.Write("Введите координату X точки: ");
+                pointX = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Введите координату Y точки: ");
+                pointY = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Введите координату X центра круга: ");
+                centerX = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Введите координату Y центра круга: ");
+                centerY = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Введите радиус круга: ");
+                radius = Convert.ToDouble(Console.ReadLine());
+
+                distance = Math.Sqrt(Math.Pow(pointX - centerX, 2) + Math.Pow(pointY - centerY, 2));
+                if (distance <= radius)
+                {
+                    Console.WriteLine("Принадлежит");
+                }
+                else
+                {
+                    Console.WriteLine("Не принадлежит");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Введено не число");
+            }
+        }
+    }
+}
---
+++ parsing_phone_number/ConsoleApp2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string input;
+        char[] numbers = new char[12];
+        int digitCount = 0;
+        while (true)
+        {
+                Console.Write("\nДля завершения введите \'q\' \nВведите строку: ");
+            input = Console.ReadLine();
+            digitCount = 0;
+                foreach (char c in input)
+                {
+                    if (c == 'q')
+                    {
+                        return;
+                    }
+                    if (Char.IsDigit(c))
+                    {
+                        numbers[digitCount++] = c;
+                        if (digitCount == 11)
+                            break;
+                    }
+                }
+                if (digitCount < 11)
+                {
+                    Console.WriteLine("Не хватает цифр, попробуйте еще раз");
+                    break;
+                }
+
+            string number = $"+{numbers[0]}({numbers[1]}{numbers[2]}{numbers[3]}){numbers[4]}{numbers[5]}{numbers[6]}-{numbers[7]}{numbers[8]}-{numbers[9]}{numbers[10]}{numbers[11]}\n";
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter("numbers.txt", true, Encoding.UTF8))
+                {
+                    writer.Write(number);
+                }
+                Console.WriteLine("Номер успешно записана в файл!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка при записи в файл: {ex.Message}");
+            }
+        }
+    }
+}
